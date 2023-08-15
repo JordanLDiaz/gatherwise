d618e132b0e107bebcffb14cc4f1bd92888a63c3
Jordan Diaz <jordanldiaz29@gmail.com>
2023-08-15 15:22:01 -0600

    commit

--- Tower2.client/vite.config.js
+++ Tower2.client/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: '../Tower2/client',
+    outDir: 'docs',
     sourcemap: false
   },
   server: {
